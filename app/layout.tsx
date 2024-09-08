2bd2712825656d05430f9722db3cc40ef4af9955
SnoopyKim <snoopykim.dev@gmail.com>
2024-09-08 09:28:14 +0900

    Initial Setting: Add Supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { pretendard } from "@/app/_assets/fonts";
+import { pretendard } from "@assets/fonts";
 
 export const metadata: Metadata = {
   title: "ChartQ",
